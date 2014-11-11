f28c08ead15aa0d28c88bee4eb6209a9ce4e8990
me10zyl@qq.com <me10zyl@qq.com>
2014-11-11 18:00:09 +0800

    auto push#2014-11-11+18:00:08

---
+++ html5demos/13/scripts/chat.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2014/11/11.
+ */
+var ws = require("websocket-server");
+var server = ws.createServer();
+//websocket连接事件.[添加事件]
+server.addListener("connection",function(conn){
+    conn.addListener("message",function(msg){
+        console.log(msg);
+        server.broadcast(msg);
+    })
+});
+var port = 8899;
+server.listen(port);
+console.log("server is running on port "+port);
